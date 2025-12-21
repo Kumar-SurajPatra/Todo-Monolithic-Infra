434f5f04d156a6d8a944dbba91c7fbda225fc818
Kumar-SurajPatra <142219708+Kumar-SurajPatra@users.noreply.github.com>
2025-12-21 19:06:56 +0530

    Update main.tf

--- modules/azurerm_public_ip/main.tf
+++ modules/azurerm_public_ip/main.tf
@@ -1,39 +1,11 @@
-#resource "azurerm_public_ip" "pip"{
-#    for_each = var.public_ip
-#    name = each.value.name
-#    resource_group_name = each.value.rg_name
-#    location = each.value.location
-#    allocation_method = each.value.allocation_method
-#
-#    sku = lookup(each.value,"sku","Standard")
-#    sku_tier = lookup(each.value,"sku_tier","Regional")
-#
-#    zones = length(lookup(each.value, "zones", [])) > 0 ? each.value.zones : null
-#
-#    ip_version = lookup(each.value,"ip_version","IPV4")
-#
-#    ddos_protection_plan_id = (
-#    lookup(each.value, "ddos_protection_mode", "VirtualNetworkInherited") == "Enabled" ?
-#    lookup(each.value, "ddos_protection_plan_id", null) :
-#    null)
-#
-#    edge_zone               = lookup(each.value, "edge_zone", null)
-#  idle_timeout_in_minutes = lookup(each.value, "idle_timeout_in_minutes", 4)
-#  ip_tags                 = lookup(each.value, "ip_tags", {})
-#  public_ip_prefix_id     = lookup(each.value, "public_ip_prefix_id", null)
-#  reverse_fqdn            = lookup(each.value, "reverse_fqdn", null)
-#
-#  # Merge default + custom tags
-#  #tags = merge(local.default_tags, lookup(each.value, "tags", {}))
-#
-#  lifecycle {
-#    create_before_destroy = true
-#    ignore_changes = [
-#      tags["last_modified"], # example of dynamic tag ignore
-#    ]
-#  }
-#
-#  depends_on = [
-#    # future dependency placeholder
-#  ]
-#}
+resource "azurerm_public_ip" "pip" {
+  name                    = var.public_ip_name
+  location                = var.public_ip_location
+  resource_group_name     = var.resource_group_name
+  allocation_method       = var.allocation_method
+  idle_timeout_in_minutes = 30
+
+  tags = {
+    environment = "test"
+  }
+}
