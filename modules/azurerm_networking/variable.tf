209bd39802c21bdaaba800a8c5c072454baffdc8
Kumar-SurajPatra <142219708+Kumar-SurajPatra@users.noreply.github.com>
2025-12-21 19:18:16 +0530

    Update variable.tf

--- modules/azurerm_networking/variable.tf
+++ modules/azurerm_networking/variable.tf
@@ -1,13 +1,19 @@
-#variable vnets{
-#    type = map(object({
-#        name = string
-#        location = string
-#        rg_name = string
-#        address_space = list(string)
-#        tags = optional(map(string))
-#        subnets = optional(list(object9({
-#            name = string
-#            address_prefixes = list(string)
-#        })),[])        
-#    }))
-#}
+variable vNet_name{
+    type = string
+    description = "vNet ka naam"
+}
+
+variable vNet_location{
+    type = string
+    description = "vNet ka location"
+}
+
+variable vNet_rg_name{
+    type = string
+    description = "vNet konse rg me he uska naam"
+}
+
+variable vNet_address_space{
+    type = list(string)
+    description = "vNet ka address space"
+}
