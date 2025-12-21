49ac0208921f29c81ccb1623c037be090e3ef551
Kumar-SurajPatra <142219708+Kumar-SurajPatra@users.noreply.github.com>
2025-12-21 19:16:05 +0530

    Update main.tf

--- modules/azurerm_resource_group/main.tf
+++ modules/azurerm_resource_group/main.tf
@@ -1,7 +1,4 @@
-resource "azurerm_resource_group" "rgs"{
-    for_each = var.rgs
-    name = each.value.name
-    location = each.value.location
-    managed_by = lookup(each.value, "managed_by", "terraform")
-    tags = each.value.tags
+resource "azurerm_resource_group" "res-grp" {
+  name = var.resource_group_name
+  location = var.resource_group_location
 }
