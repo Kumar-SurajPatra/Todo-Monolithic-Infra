79b5660db8cca632b39b9902cd944f15418a5ad1
Kumar-SurajPatra <142219708+Kumar-SurajPatra@users.noreply.github.com>
2025-12-21 19:17:48 +0530

    Update main.tf

--- modules/azurerm_networking/main.tf
+++ modules/azurerm_networking/main.tf
@@ -1,18 +1,6 @@
-#esource "azurerm_virtual_network" "vnets"{
-#   for_each = var.vnets
-#   name = each.value.vnet_name
-#   location = each.value.location
-#   resource_group_name = each.value.rg_name
-#   address_space = each.value.address_space
-#   tags = each.value.tags
-#   #dns_servers = each.value.dns_servers
-#   #private_endpoint_vnet_policies = each.value.private_endpoint_vnet_policies
-#
-#   dynamic "subnet"{
-#       for_each = each.value.subnets
-#       content{
-#           name = subnet.value.name
-#           address_prefixes = subnet.value.address_prefixes
-#       }
-#   }
-#
+resource azurerm_virtual_network vNet {
+    name = var.vNet_name
+    resource_group_name = var.vNet_rg_name
+    location = var.vNet_location
+    address_space = var.vNet_address_space
+}
